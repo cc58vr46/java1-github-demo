6e386c115bcc71fa001c33c93d91d84da932dd2e
ccotter5 <ccotter5@MCBA202-C09.classrooms.stlcc>
2017-09-20 11:01:01 -0500

    first commit

---
+++ src/github/demo/GITHUBDemo.java
@@ -0,0 +1,16 @@
+
+package github.demo;
+
+/**
+ *
+ * @author ccotter5
+ */
+public class GITHUBDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
